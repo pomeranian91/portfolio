f97fcc394d2252b879180eaee2c00a704988d81b
pomeranian91 <forking91@gmail.com>
2021-12-16 18:02:57 +0900

    main_first

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 포트폴리오 사이트 입니다.
 HTML CSS JAVASCRIPT를 이용해서 만들었습니다.
+이미지 넣기
 주소 : https://pomeranian91.github.io/portfolio/
+
--- main.js
+++ main.js
@@ -1,12 +1,38 @@
 $(window).scroll(function(){
     var scrollHeight = $(window).scrollTop();
     console.log(scrollHeight);
-    var homeY = (-1/870) * scrollHeight + 97/87;
+    var homeY = (-1/800) * scrollHeight + 1.3;
     $('.home-box').eq(0).css('opacity', homeY);
-    var homeY2 = (-1/500) * scrollHeight + 2.85;
+    var homeY2 = (-1/500) * scrollHeight + 3;
     $('.home-box').eq(1).css('opacity', homeY2);
-    var homeY3 = (-1/500) * scrollHeight + 24/5;
+    var homeY3 = (-1/500) * scrollHeight + 4.8;
     $('.home-box').eq(2).css('opacity', homeY3);
-   
-    
+});
+
+var homeTyping = document.querySelector('.home-hello_text_first');
+var homeText = document.querySelector('.home-hello_text_first').innerHTML;
+var homeTypingSecond = document.querySelector('.home-hello_text_second');
+var homeTextSecond = document.querySelector('.home-hello_text_second').innerHTML;
+var homeTypingThird = document.querySelector('.home-hello_text_third');
+var homeTextThird = document.querySelector('.home-hello_text_third').innerHTML;
+
+window.addEventListener('load',function(){
+    homeTyping.innerHTML = '';
+    homeTypingSecond.innerHTML = '';
+    homeTypingThird.innerHTML = '';
+    for(let i = 0; i < homeText.length; i++){
+    setTimeout(function(){
+        homeTyping.innerHTML = homeTyping.innerHTML + homeText[i]
+    }, i*100);
+};
+    for(let i = 0; i < homeTextSecond.length; i++){
+    setTimeout(function(){
+        homeTypingSecond.innerHTML = homeTypingSecond.innerHTML + homeTextSecond[i]
+    }, (i+homeText.length)*100);
+};
+    for(let i = 0; i < homeTextThird.length; i++){
+    setTimeout(function(){
+        homeTypingThird.innerHTML = homeTypingThird.innerHTML + homeTextThird[i]
+    }, (i+homeText.length+homeTextSecond.length)*100);
+};
 });
