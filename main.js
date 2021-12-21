ef8ce0518c85e8a09f0f9b06da476946c6ecaddc
pomeranian91 <forking91@gmail.com>
2021-12-21 19:15:53 +0900

    skills_origin

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ $('.home__main--text2:contains("FRONT-END")').css('color','var(--y-color)');
 $(window).scroll(function(){
     var scrollHeight = $(window).scrollTop();
     console.log(scrollHeight);
-    var homeY = (-1/800) * scrollHeight + 1.3;
+    var homeY = (-1/800) * scrollHeight + 1;
     $('.home__box').eq(0).css('opacity', homeY);
     var homeY2 = (-1/500) * scrollHeight + 3;
     $('.home__box').eq(1).css('opacity', homeY2);
     var homeY3 = (-1/500) * scrollHeight + 4.8;
     $('.home__box').eq(2).css('opacity', homeY3);
 });
+//스크롤 위치에 따른 자연스럽게 사라지는 효과
 
 var homeTyping = document.querySelector('.home__main--text1');
 var homeText = document.querySelector('.home__main--text1').innerHTML;
@@ -38,3 +39,7 @@ window.addEventListener('load',function(){
     }, (i+homeText.length+homeTextSecond.length)*80);
 };
 });
+//타이핑 효과
+
+
+//스크롤 위치에 따른 자연스럽게 사라지는 효과__스킬 sticky에 사용
